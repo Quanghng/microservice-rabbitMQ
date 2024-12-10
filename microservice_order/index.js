10612852501e9ace38168e75b2f94cb2eade7e98
Quanghng <90704201+Quanghng@users.noreply.github.com>
2024-12-10 22:30:51 +0100

    Fixed bugs

--- microservice_order/index.js
+++ microservice_order/index.js
@@ -1,5 +1,7 @@
 const express = require('express')
 const mongoose = require("mongoose");
+const { consumeMessages } = require("./services/mqService");
+
 
 require("dotenv").config();
 
@@ -7,6 +9,9 @@ const app = express()
 const PORT = process.env.PORT || 3000;
 
 app.use(express.json());
+
+// RabbitMQ
+consumeMessages()
 
 mongoose
     .connect(process.env.MONGO_URI)
